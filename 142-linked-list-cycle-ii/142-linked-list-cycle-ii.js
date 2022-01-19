a3a91b895b620112777a44830789c32fb941d0ef
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-01-19 22:49:42 +0900

    Time: 116 ms (31.90%), Space: 41.6 MB (42.41%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -11,23 +11,24 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    if (head === null) {
+    if (head == null || head.next == null) {
         return null;
     }
     
-    let node = null;
-    let map = {};
-    while (head.next) {
-        if (map[head.val]) {
-            node = map[head.val].find(item => item === head);
-            map[head.val].push(head);
-        } else {
-            map[head.val] = [head];
-        }
-
-        head = head.next;
-        if (node) head.next = null;
-    }
+    let slow = head;
+    let fast = head;
+    let node = head;
     
-    return node || null;
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {                      // there is a cycle
+            while(slow != node) {               // found the entry location
+                slow  = slow.next;
+                node = node.next;
+            }
+            return node;
+        }
+    }
+    return null;    
 }; 
