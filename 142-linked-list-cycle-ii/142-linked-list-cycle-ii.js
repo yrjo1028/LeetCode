752b19f8b66b1d2303d2083a1cea3536a78616d3
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-01-19 19:15:43 +0900

    Time: 869 ms (5.00%), Space: 41.6 MB (42.41%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    
+    let start = null;
+    let arr = [];
+    
+    while (head?.next) {
+        start = arr.find(cur => cur === head.next);
+        if (start) {
+            head.next = null;
+        } else {
+            arr.push(head);
+            head = head.next;
+        }
+    }
+    
+    return start || null;
+    
+};
