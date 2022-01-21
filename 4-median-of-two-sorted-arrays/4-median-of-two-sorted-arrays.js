2f96e2e3b5fd2c73e03881ca4b8a33dbbe0503b6
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-01-21 16:00:10 +0900

    Time: 224 ms (24.94%), Space: 45.1 MB (42.24%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    let arr = nums1.concat(nums2).sort(function(a, b) { return a - b});
+    let arr = nums1.concat(nums2);
+    arr.sort(function(a, b) { return a - b});
     let idx = Math.ceil(arr.length / 2);
-    
     return (arr[idx - 1] + arr[arr.length - idx]) /2
 };
