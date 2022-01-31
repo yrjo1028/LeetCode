d729182f15b757409aa7bc832014a8504360f2b0
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-01-31 19:31:02 +0900

    Time: 119 ms (37.04%), Space: 44.9 MB (5.09%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -11,17 +11,20 @@
  */
 var deleteDuplicates = function(head) {
     
+    // 리스트가 1개 이하 일 경우 그대로 리턴
     if (head === null || head.next === null) {
         return head;
     }
     
+    // 중복제거할 리스트와 포인터 생성
     let arr = null;
     let p = null;
     
+    // 주어진 배열을 모두 조회할 때 까지 반복
     while (head) {
         let val = head.val;
         if (!head.next || val !== head.next.val) {
-            let node = new ListNode(head.val);
+            let node = new ListNode(val);
             if (p) {
                 p.next = node;
                 p = p.next;
