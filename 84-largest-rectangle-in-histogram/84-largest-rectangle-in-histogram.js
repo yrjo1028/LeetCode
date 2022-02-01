516d4da9da510dcc963e9f3fc250c04de4baf11f
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-02-01 11:53:32 +0900

    Time: 5580 ms (5.05%), Space: 53.3 MB (44.39%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    
+    let lagest = 0;
+    let p = 0;
+    let bf = [];
+    
+    while (heights.length) {
+        
+        let cnt = 1;
+        
+        p = heights.shift();
+        heights.some(cur => {
+            if (p > cur) {
+                return true;
+            }
+            cnt++;
+        });
+        
+        bf.some(cur => {
+            if (p > cur) {
+                return true;
+            }
+            cnt++;
+        });
+        
+        let sum = p * cnt;
+        if (sum > lagest) lagest = sum;
+        
+        bf.unshift(p);
+        while(p === heights[0]) {
+            bf.unshift(heights.shift());
+        }
+    }
+    
+    return lagest;
+    
+};
