ae96d98496002b4a6c7effb4e9bd030306d8979a
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-02-05 16:10:57 +0900

    Time: 112 ms (32.27%), Space: 42.6 MB (9.26%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,15 +4,15 @@
  */
 var maxProfit = function(prices) {
     
-    if (2 > prices.length) {
-        return 0;
-    }
+    if (2 > prices.length) return 0;
     
     let maxi = 0;
+    let hold = 0;
+    let target = 0;
     
     while (prices.length) {
-        let hold = prices.shift();
-        let target = hold;
+        hold = prices.shift();
+        target = hold;
         
         while (prices.length && prices[0] >= target) {
             target = prices.shift();
