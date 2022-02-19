0a3698b6d3e773845704a62d60349d57ea0278f9
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-02-19 16:28:15 +0900

    Time: 286 ms (55.29%), Space: 63.6 MB (74.39%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.js
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.js
@@ -4,28 +4,22 @@
  */
 var maxPoints = function(points) {
     let ans = 0;
-    let m = points.length;
-    let n = points[0].length;
+    let col = points[0].length;
     
-    let dp = [];
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if (!dp[j]) dp[j] = 0;
-            dp[j] += points[i][j];
-        }
+    const dp = new Array(col).fill(0);
+    points.forEach((arr, i) => {
+        arr.forEach((num, j) => {
+           dp[j] += num; 
+        });
         
-        for(let j=1; j<n; j++){
+        for(let j=1; j < col; j++){
             dp[j] = Math.max(dp[j], dp[j-1] - 1);
         }
         
-        for(let j=n-2; j>=0; j--){
+        for(let j=col-2; j>=0; j--){
             dp[j] = Math.max(dp[j], dp[j+1] - 1);
         }
-    }
+    });
     
-    for(let i=0; i<n; i++){
-        ans = Math.max(ans, dp[i]);
-    }
-    
-    return ans;
+    return Math.max(...dp);
 };
