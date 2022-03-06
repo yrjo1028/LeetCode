1947eeec8b8bb08e420b415a4afb01c54ed40918
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-03-06 16:13:32 +0900

    Time: 2484 ms (5.12%), Space: 61.1 MB (34.85%) - LeetHub

---
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} heights
+ * @return {number[]}
+ */
+var findBuildings = function(heights) {
+    
+    let ans = [];
+    
+    let n = heights.length;
+    let taller = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        if (heights[n - i] > taller) {
+            taller = heights[n - i];
+            ans.unshift(n - i);
+        }
+    }
+    
+    return ans;
+    
+};
