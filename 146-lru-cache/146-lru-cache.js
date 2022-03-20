b114f65b17a9ef8bb6452c7d08482ab5f98a1cac
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-03-20 16:34:02 +0900

    Time: 698 ms (60.20%), Space: 128 MB (9.14%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -1,31 +1,61 @@
-class ListNode {
-    constructor(key, val, next) {
-        this.key = key ?? 0;
-        this.val = val ?? 0;
-        this.next = next || null;
-    }
-}
 /**
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-    this.size = 0;
+    this.ListNode = function (key, value, prev, next) {
+        this.key = (key===undefined ? null : key)
+        this.value = (value===undefined ? null : value)
+        this.prev = (prev===undefined ? null : prev)
+        this.next = (next===undefined ? null : next)
+    }
+    
     this.capacity = capacity;
-    this.prevMap = new Map();
-    this.dummy = new ListNode();
-    this.tail = this.dummy;
+    this.map = {};
+    this.size = 0;
+    this.head = new this.ListNode();
+    this.tail = new this.ListNode();
+    
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
 };
+
+LRUCache.prototype.addNode = function(node) {
+    node.prev = this.head;
+    node.next = this.head.next;
+    
+    this.head.next.prev = node;
+    this.head.next = node;
+}
+
+LRUCache.prototype.removeNode = function(node) {
+    let prev = node.prev;
+    let next = node.next;
+    
+    prev.next = next;
+    next.prev = prev;
+}
+
+LRUCache.prototype.popTail = function(node) {
+    let res = this.tail.prev;
+    this.removeNode(res);
+    return res;
+}
+
+LRUCache.prototype.moveToHead = function (node) {
+    this.removeNode(node);
+    this.addNode(node);
+}
 
 /** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (!this.prevMap.has(key)) {
-        return -1;
-    }
-    this.moveToTail(key);
-    return this.tail.val;
+    let node = this.map[key];
+    if (node == null) return -1;
+    
+    this.moveToHead(node);
+    return node.value;
 };
 
 /** 
@@ -34,43 +64,24 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    if (this.prevMap.has(key)) {
-        this.moveToTail(key);
-        this.tail.val = value;
-        return;
+    let node = this.map[key];
+    if (node == null) {
+        node = new this.ListNode(key, value);
+        this.map[key] = node;
+        this.addNode(node);
+        
+        this.size++;
+        
+        if (this.size > this.capacity) {
+            let tail = this.popTail();
+            delete this.map[tail.key];
+            this.size--;
+        }
+    } else {
+        node.value = value;
+        this.moveToHead(node);
     }
-    
-    if (this.size < this.capacity) {
-        this.size += 1;
-        const node = new ListNode(key, value);
-        this.tail.next = node;
-        this.prevMap.set(key, this.tail);
-        this.tail = node;
-        return;
-    }
-    
-    const first = this.dummy.next;
-    this.prevMap.delete(first.key);
-    first.key = key;
-    first.val = value;
-    this.prevMap.set(key, this.dummy);
-    this.moveToTail(key);
 };
-
-LRUCache.prototype.moveToTail = function(key) {
-    const prev = this.prevMap.get(key);
-    const curr = prev.next;
-    if (curr === this.tail) {
-        return;
-    }
-    
-    prev.next = prev.next.next;
-    this.tail.next = curr;
-    curr.next = null;
-    this.prevMap.set(curr.key, this.tail);
-    this.prevMap.set(prev.next.key, prev);
-    this.tail = curr;
-}
 
 /** 
  * Your LRUCache object will be instantiated and called as such:
