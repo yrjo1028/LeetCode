1ce106083cae9ed60a71a48a2d3484fe32a86845
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-06-19 15:36:52 +0900

    Time: 120 ms (80.22%), Space: 46.3 MB (65.30%) - LeetHub

---
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var uniqueLetterString = function(s) {
+    const lastPositions = new Array(26).fill(0);
+    const contributions = new Array(26).fill(0);
+    
+    let i = 0;
+    let answer = 0;
+    let cur = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const charIndex = i + 1;
+        const code = s[i].charCodeAt() - 65;
+        const add = charIndex - lastPositions[code];
+        const minus = contributions[code];
+        
+        cur = cur + add - minus;
+        answer += cur;
+        
+        contributions[code] = add;
+        lastPositions[code] = charIndex;
+    }
+    
+    return answer;
+};
+
+// var uniqueLetterString = function(s) {
+//     let answer = 0;
+//     for (let i = 0 ; i < s.length; i++) {
+//         let count = 0;
+//         let map = {};
+//         for (let j = i; j < s.length; j++) {
+//             char = s[j];
+//             if (map[char] === undefined) {
+//                 map[char] = 0;
+//                 count++;
+//             } else if (map[char] === 0) {
+//                 map[char]++;
+//                 count--;
+//             }
+//             answer += count;
+//         }
+//     }
+//     return answer;
+// };
