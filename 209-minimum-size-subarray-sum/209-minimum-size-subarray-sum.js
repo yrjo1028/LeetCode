e205f172dbdb99d5bad81be8f8544ad3426d3578
조영래 <91712564+yrjo1028@users.noreply.github.com>
2022-09-03 18:23:16 +0900

    Time: 105 ms (45.52%), Space: 46 MB (83.07%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let minLength = nums.length + 1;
+    let p1 = -1;
+    let p2 = -1;
+    
+    let sum = 0;
+    while (nums.length > p1 && nums.length > p2) {
+        if (nums.length > p1) {
+            p1++;
+            sum += nums[p1];
+        }
+        
+        while (sum >= target) {
+            if (sum >= target) {
+                const length = p1 - p2;
+                minLength = Math.min(length, minLength);
+            }
+            
+            p2++;
+            sum -= nums[p2];
+        }
+    }
+    
+    if (minLength > nums.length) {
+        minLength = 0;
+    }
+    return minLength;
+};
